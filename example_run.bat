77c75700598ec94838691e6eb1472d8357d7f993
Graham Ward <grahamward@me.com>
2025-02-23 15:35:03 -0800

    changed the name of the main notebook and altered the automation code to reflect this.

--- example_run.bat
+++ example_run.bat
@@ -7,9 +7,9 @@ REM activate main anaconda environment
 call "C:\Users\Username\anaconda3\Scripts\activate.bat" main
 
 REM run Jupyter notebook
-jupyter nbconvert --to notebook --execute notebook_name.ipynb --stdout > run_log.txt 2>&1
+jupyter nbconvert --to notebook --execute notebook_name.ipynb --stdout > run_logs.txt 2>&1
 
 REM deactivate main anaconda environment
 call conda deactivate
 
-echo Task completed at %DATE% %TIME% >> run_log.txt
+echo Task completed at %DATE% %TIME% >> run_logs.txt
